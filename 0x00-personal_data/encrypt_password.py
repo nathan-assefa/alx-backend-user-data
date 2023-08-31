26a3a00e885b85a33fc2264232f45e77c927dd19
nathan-assefa <aasamnew@gmail.com>
2023-08-31 11:52:04 +0000

    personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,8 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' Verify if the password matches the hashed password '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
