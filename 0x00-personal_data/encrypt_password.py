bc0c327ffe78ac821db23d47bcca8789949d7da1
nathan-assefa <aasamnew@gmail.com>
2023-08-31 11:18:41 +0000

    personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,4 +2,15 @@
 ''' Encrypting password '''
 
 
+import bcrypt
 
+
+def hash_password(password: str) -> bytes:
+    ''' Encrypting password '''
+    # Generate a salt using gensalt method
+    salt = bcrypt.gensalt()
+
+    # Hash the password using the generated salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
