b356b63e8267fc413e04dc8423f16029658276aa
nathan-assefa <aasamnew@gmail.com>
2023-09-13 13:05:30 +0000

    user auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,5 +87,22 @@ def profile():
     abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Generate a token for resetting a user's password
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        response = {
+                "email": email,
+                "reset_token": reset_token
+                }
+        return jsonify(response)
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
