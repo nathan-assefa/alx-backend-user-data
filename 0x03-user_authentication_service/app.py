13b7ecda900f3c01e4a37c3e3dad5ba3dfcbefd8
nathan-assefa <aasamnew@gmail.com>
2023-09-13 10:10:53 +0000

    user auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Basic flask app """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
