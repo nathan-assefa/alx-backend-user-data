1066744ea3167f41feee4fe7f90fb3157470d62c
nathan-assefa <aasamnew@gmail.com>
2023-09-13 13:18:57 +0000

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,7 +103,8 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
-
+    
+    '''
     def update_password(self, reset_token: str, password: str) -> None:
         """
         Updates a user's password
@@ -120,3 +121,20 @@ class Auth:
                     )
         except NoResultFound:
             raise ValueError
+    '''
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Updates a user's password
+        Args:
+            reset_token (str): reset_token issued to reset the password
+            password (str): user's new password
+        Return:
+            None
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError()
+
+        hashed = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
